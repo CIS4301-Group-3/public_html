d63156264a53d82a05a4470fd4eac1acd37d312d
Kevin P. Aukee <aukee@thunder.cise.ufl.edu>
2020-04-10 00:34:00 -0400

    Figured out how to get the stylesheets to work!

--- private/initialize.php
+++ private/initialize.php
@@ -18,7 +18,7 @@
     // * Can dynamically find everything in URL up to "/public"
     $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
     $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
-    define("WWW_ROOT", 'http://www.cise.ufl.edu' . $doc_root);
+    define("WWW_ROOT", $doc_root);
 
 
     require_once('functions.php');
