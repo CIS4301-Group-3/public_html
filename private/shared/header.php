aadcb00fe465f97eeaafc54dca1811c6c7deb193
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-11 14:18:44 -1000

    Added bootstrap functionality

--- private/shared/header.php
+++ private/shared/header.php
@@ -8,7 +8,10 @@
   <head>
     <title>EFI - <?php echo h($page_title); ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/site.css'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--<link rel="stylesheet" media="all" href="<?php //echo url_for('/stylesheets/site.css'); ?>" /> -->
   </head>
 
   <body>
