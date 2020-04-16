4b39e521a7774f6feb6a6749f56ac3f1ab76e909
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-16 11:04:17 -1000

    added donations over time query take 8

--- public/cand_comp.php
+++ public/cand_comp.php
@@ -11,7 +11,7 @@
   </div>
 </div>
 <?php
-  $query = oci_parse($db, 'SELECT * FROM aukee.employer ORDER BY rank ASC');
+  $query = oci_parse($db, 'SELECT * FROM aukee.employer ORDER BY FORTUNE_RANK ASC');
   oci_execute($query);
   echo "<div><br>
           <hr><table>\n";
