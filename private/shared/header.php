58324f23897801a1ac41be896fec0e1085029653
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-16 17:53:07 -1000

    added donations over time query take 25

--- private/shared/header.php
+++ private/shared/header.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script type="text/javascript">
-      var jsonArray = <?php echo json_encode($dataPoints); ?>
+      var jsonArray = <?php echo json_encode($dataPoints); ?>;
       var newJSONArray = [];
       var date;
       var year;
