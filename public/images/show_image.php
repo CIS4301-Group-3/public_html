f3f3afa00c004162caeb22167e80dafe35d4d7d5
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-17 12:30:22 -1000

    messing with the CSS

--- public/images/show_image.php
+++ public/images/show_image.php
@@ -5,6 +5,7 @@
 if(isset($_GET['id']))
 {
   $id = urldecode($_GET['id']);
+  echo $id;
   $query = candidate_photo($id);
   while($row = oci_fetch_array($query, OCI_ASSOC+OCI_RETURN_NULLS)) {
     $imageData = $row['IMAGE'];
--- public/ind_cand_stats.php
+++ public/ind_cand_stats.php
@@ -58,7 +58,8 @@
   <div class="col-2">
     <h4>Choose a Candidate</h4>
     <img src="images/show_images.php?id=Bernie+Sanders" alt="Bernie Sanders" class="img-thumbnail">
-    <?php /*
+    <?php /* // Trying to make code to dynamically add the candidates as they are added to the
+            // database
       for($i=0;$i<count($candidate_array);$i++) {
         echo "<img src=\"";
         echo url_for('/images/show_image.php?id=' . h(u($candidate_array[i]['CANDIDATE'])));
