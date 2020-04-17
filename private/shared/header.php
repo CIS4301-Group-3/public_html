f67a2501dcca6a6e6ce709b701b1b90c45329929
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-16 19:46:15 -1000

    added donations over time query take 34

--- private/shared/header.php
+++ private/shared/header.php
@@ -11,42 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script type="text/javascript">
-      var jsonArray = <?php echo json_encode($dataPoints); ?>;
-      var newJSONArray = [];
-      var date;
-      var year;
-      var month;
-      var day;
-      for (var i=0;i<jsonArray.length;i++)
-      {
-        date = jsonArray[i]['YEAR_DATE'];
-        year = parseInt(date.substring(0, 4));
-        month = parseInt(date.substring(4, 6)) - 1;
-        day = parseInt(date.substring(6, 8));
-        newJSONArray.push({x: new Date(year, month, day), y: parseInt(jsonArray[i]['TOTAL_DONATIONS'])});
-      }
-      console.log(newJSONArray);
-      window.onload = function () {
-      
-      var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        exportEnabled: true,
-        title: {
-          text: "<?php echo $candidate?> Donations Over Time"
-        },
-        axisY: {
-          title: "Amount (USD)"
-        },
-        data: [{
-          type: "line",
-          dataPoints: newJSONArray
-        }]
-      });
-      chart.render();
-      
-      }
-</script>
   </head>
 
   <body>
--- public/ind_cand_stats.php
+++ public/ind_cand_stats.php
@@ -13,6 +13,43 @@
 <?php $page_title = 'Individual Stats'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
+<script type="text/javascript">
+      var jsonArray = <?php echo json_encode($dataPoints); ?>;
+      var newJSONArray = [];
+      var date;
+      var year;
+      var month;
+      var day;
+      for (var i=0;i<jsonArray.length;i++)
+      {
+        date = jsonArray[i]['YEAR_DATE'];
+        year = parseInt(date.substring(0, 4));
+        month = parseInt(date.substring(4, 6)) - 1;
+        day = parseInt(date.substring(6, 8));
+        newJSONArray.push({x: new Date(year, month, day), y: parseInt(jsonArray[i]['TOTAL_DONATIONS'])});
+      }
+      console.log(newJSONArray);
+      window.onload = function () {
+      
+      var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+          text: "<?php echo $candidate?> Donations Over Time"
+        },
+        axisY: {
+          title: "Amount (USD)"
+        },
+        data: [{
+          type: "line",
+          dataPoints: newJSONArray
+        }]
+      });
+      chart.render();
+      
+      }
+</script>
+
 <div id="content">
   <div id="main-menu">
     <h2>Individual Candidate Statistics</h2>
