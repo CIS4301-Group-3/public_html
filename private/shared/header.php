f8fae96daadd5889dd731238c8d2979605305069
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-17 18:33:39 -1000

    messing with us map

--- private/shared/header.php
+++ private/shared/header.php
@@ -15,8 +15,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="js/raphael.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
+    <script src="js/raphael.js"></script>
     <script src="js/color.jquery.js"></script>
     <script src="js/jquery.usmap.js"></script>
   </head>
