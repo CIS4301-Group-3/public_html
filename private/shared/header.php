b561909882a3fedec9e5d1e2e2cc41d001b2defc
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-20 22:28:41 -1000

    messing with css

--- private/shared/header.php
+++ private/shared/header.php
@@ -30,7 +30,7 @@
     <header>
       <h1>Election Finance Insights</h1>
     </header>
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar navbar-expand-lg navbar-light custom-navbar">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active"><a class="nav-link" href="<?php echo url_for('/index.php'); ?>">Home</a></li>
         <li class="nav-item active"><a class="nav-link" href="<?php echo url_for('/cand_comp.php'); ?>">Candidate Comparison</a></li>
