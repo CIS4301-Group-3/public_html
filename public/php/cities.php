1da87ea398681d4a862f35568dca12296f1d9f4c
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-20 19:10:52 -1000

    messing with city input

--- public/php/cities.php
+++ public/php/cities.php
@@ -7,11 +7,6 @@
   $state = $_GET['state'];
   $query = get_cities($state);
   $nrows = oci_fetch_all($query, $city_list, null, null, OCI_FETCHSTATEMENT_BY_ROW+OCI_ASSOC);
-  echo $city_list;
-?>
+  echo json_encode($city_list);
 
-<script>
-  var citiesJSON = <?php echo json_encode($city_list); ?>;
-  console.log("List of Cities");
-  console.log(citiesJSON);
-</script>
+?>
