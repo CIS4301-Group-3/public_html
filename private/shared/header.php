759f69f87dc6fe3769d8035104eb463446d9cbdd
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-20 20:09:52 -1000

    messing with city input

--- private/shared/header.php
+++ private/shared/header.php
@@ -16,6 +16,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo url_for('/stylesheets/style.css'); ?>">
     <script src="js/jquery-3.5.0.min.js"></script>
     <script src="js/raphael.js"></script>
