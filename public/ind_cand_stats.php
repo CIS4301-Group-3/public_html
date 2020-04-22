15bb7337c7137db5cc90bba9c5911c676766a6f8
Kevin Aukee <kevin.aukee@gmail.com>
2020-04-21 17:09:47 -1000

    Adding Individual Stats

--- public/ind_cand_stats.php
+++ public/ind_cand_stats.php
@@ -137,6 +137,7 @@
           axisY: {
             title: "Donations (in USD)",
             prefix: "$",
+            titleFontSize: 20;
           },
           data: [{
             type: "bar",
